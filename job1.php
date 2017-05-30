fa3808bb47e5792be22bdbd3318dde713224c531
fajarhastunaedi <fajarhastunaedi11@gmail.com>
2017-05-30 10:38:02 +0700

    Add files via upload

---
+++ job1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Contoh pemakaian Script PHP</title>
+</head>
+<body>
+<? //register global on
+echo "sekarang aku udah gede! <br>";
+print	"mudah dan menyenangkan <br>";
+?>
+<script language="php">
+echo ("tulis pake ini jika html editor tidak mengenali PHP");
+</script>
+</body>
+</html>
